d6f930240fe0ee0758067e0b09acd43a844f43da
Stephen McGill <stephen.mcgill@tri.global>
2018-05-08 12:44:11 -0400

    Update getch module

---
+++ Modules/lua-getch/getch-0.1-0.rockspec
@@ -0,0 +1,35 @@
+package = "getch"
+version = "0.1-0"
+source = {
+  url = "git://github.com/StephenMcGill-TRI/lua-getch.git"
+}
+description = {
+  summary = "Access tty",
+  detailed = [[
+      Access tty from Lua
+    ]],
+  homepage = "https://github.com/StephenMcGill-TRI/lua-getch",
+  maintainer = "Stephen McGill <stephen.mcgill@tri.global>",
+  license = "MIT"
+}
+dependencies = {
+  "lua >= 5.1",
+}
+build = {
+  type = "make",
+  build_variables = {
+    CFLAGS="$(CFLAGS)",
+    LIBFLAG="$(LIBFLAG)",
+    LUA_LIBDIR="$(LUA_LIBDIR)",
+    LUA_BINDIR="$(LUA_BINDIR)",
+    LUA_INCDIR="$(LUA_INCDIR)",
+    LUA="$(LUA)",
+  },
+  install_variables = {
+    INST_PREFIX="$(PREFIX)",
+    INST_BINDIR="$(BINDIR)",
+    INST_LIBDIR="$(LIBDIR)",
+    INST_LUADIR="$(LUADIR)",
+    INST_CONFDIR="$(CONFDIR)",
+  },
+}
--- Modules/lua-getch/lua_getch.c
+++ Modules/lua-getch/lua_getch.c
@@ -2,33 +2,33 @@
   Lua module to provide keyboard input
 */
 
+#include <fcntl.h>
 #include <stdio.h>
 #include <termios.h>
-#include <fcntl.h>
 
-#include "lua.h"
-#include "lualib.h"
-#include "lauxlib.h"
+#include <lauxlib.h>
+#include <lua.h>
+#include <lualib.h>
 
-
-static int luagetch_block(lua_State *L){
+static int luagetch_block(lua_State *L) {
   struct termios oldt, newt;
   int ch, oldf;
 
-  tcgetattr(0, &oldt );
+  tcgetattr(0, &oldt);
   newt = oldt;
   newt.c_lflag &= ~(ICANON | ECHO);
-  tcsetattr(0, TCSANOW, &newt );
+  tcsetattr(0, TCSANOW, &newt);
   oldf = fcntl(0, F_GETFL, 0);
   fcntl(0, F_SETFL, oldf & ~O_NONBLOCK);
   ch = getchar();
-  tcsetattr(0, TCSANOW, &oldt );
+  tcsetattr(0, TCSANOW, &oldt);
   fcntl(0, F_SETFL, oldf);
 
-  if (ch >= 0)
+  if (ch >= 0) {
     lua_pushinteger(L, ch);
-  else
+  } else {
     lua_pushnil(L);
+  }
   return 1;
 }
 
@@ -46,24 +46,22 @@ static int luagetch_nonblock(lua_State *L) {
   tcsetattr(0, TCSANOW, &oldt);
   fcntl(0, F_SETFL, oldf);
 
-  if (ch >= 0)
+  if (ch >= 0) {
     lua_pushinteger(L, ch);
-  else
+  } else {
     lua_pushnil(L);
+  }
   return 1;
 }
 
-static const struct luaL_Reg getch_lib [] = {
-  {"block", luagetch_block},
-  {"nonblock", luagetch_nonblock},
-  {NULL, NULL}
-};
+static const struct luaL_Reg getch_lib[] = {
+    {"block", luagetch_block}, {"nonblock", luagetch_nonblock}, {NULL, NULL}};
 
-int luaopen_getch (lua_State *L) {
-#if LUA_VERSION_NUM == 502
-	  luaL_newlib(L, getch_lib);
+int luaopen_getch(lua_State *L) {
+#if LUA_VERSION_NUM == 501
+  luaL_register(L, "getch", getch_lib);
 #else
-	luaL_register(L, "getch", getch_lib);
+  luaL_newlib(L, getch_lib);
 #endif
   return 1;
 }
